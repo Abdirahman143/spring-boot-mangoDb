9ba51ca0a982b7f296f95d28cd7de0bd92ca1694
Abdi <KEN20963@kcbgroup.com>
2023-03-20 11:07:35 +0300

    Initial commit

---
+++ src/test/java/com/cbc/mongo/springbootmangodb/SpringBootMangoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbc.mongo.springbootmangodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMangoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
